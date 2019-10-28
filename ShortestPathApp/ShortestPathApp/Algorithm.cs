0adb5033234ff40e8d2f415c3cf9660b4161f439
bukajj <42746240+bukajj@users.noreply.github.com>
2019-10-28 22:05:42 +0100

    ShortestPath
    Not finished

---
+++ ShortestPathApp/ShortestPathApp/Algorithm.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShortestPathApp
+{
+    class Algorithm
+    {
+        Point[] points;
+        public Algorithm(Point[] points)
+        {
+            this.points = points;
+        }
+        
+        private int CountPath(Point point)
+        {
+            int path = Int32.MaxValue;
+            
+            foreach(Point item in this.points)
+            {
+                int newPath = 0;
+                while()
+                newPath += Math.Abs(point.x - item.x) + Math.Abs(point.y - item.y);
+            }
+            return path;
+        }
+
+        public int ShortestPathAlgorithm()
+        {
+           
+            
+            foreach (Point item in this.points)
+            
+            return path;
+        }
+
+
+    }
+}
