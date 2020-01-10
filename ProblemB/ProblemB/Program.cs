14889d5d8313eae79e72df4e5456136552e377b4
bukajj <42746240+bukajj@users.noreply.github.com>
2020-01-10 14:49:43 +0100

    asdadas

--- ProblemB/ProblemB/Program.cs
+++ ProblemB/ProblemB/Program.cs
@@ -6,19 +6,40 @@ namespace ProblemB
     {
         static void Main(string[] args)
         {
-            Point point1 = new Point(-5, 1);
-            Point point2 = new Point(-2, 2);
-            Point point3 = new Point(1, 3);
-            Point point4 = new Point(4, 4);
-            Point point5 = new Point(1, 2);
-            Point point6 = new Point(1, -1);
-            Point point7 = new Point(-8, 0);
+            string currentLine = Console.ReadLine();
+            int mainCounter = 0;
 
-            Point[] points = { point1, point2, point3, point4, point5, point6, point7 };
-            
-            Algorithm algorithm = new Algorithm(points);
+            while(!currentLine[0].Equals('-') && !currentLine[1].Equals('-'))
+            {
+                Point[] points = new Point[1000];
+                int counter = 0;
 
-            Console.WriteLine(algorithm.maxValue().ToString());
-        }
+                while (!currentLine[0].Equals('-') && !currentLine[1].Equals('-'))
+                {
+                    string[] text = currentLine.Split(new char[] { ' ' });
+                    int[] point_args = new int[2];
+
+                    for (int i=0; i<2; i++)
+                    {
+                        point_args[i] = Convert.ToInt32(text[i]);
+                    }
+
+                    points[counter] = new Point(point_args[0], point_args[1]);
+                    counter++;
+                    currentLine = Console.ReadLine();
+                }
+
+                Point[] new_points = new Point[counter];
+                for (int i=0; i<new_points.Length; i++)
+                {
+                    new_points[i] = points[i];
+                }
+
+                Algorithm algorithm = new Algorithm(new_points);
+                Console.WriteLine((mainCounter+1).ToString() + ". " + algorithm.maxValue().ToString());
+                currentLine = Console.ReadLine();
+                mainCounter++;
+            }
+        }   
     }
 }
--- ProblemC/ProblemC/Program.cs
+++ ProblemC/ProblemC/Program.cs
@@ -6,10 +6,44 @@ namespace ProblemC
     {
         static void Main(string[] args)
         {
-            Point startPoint = new Point(2, 2);
-            Point endPoint = new Point(8, -100);
-            Algorithm algorithm = new Algorithm(startPoint, endPoint);
-            Console.WriteLine(algorithm.JumpsAlgorithm());
+            int tests = Convert.ToInt32(Console.ReadLine());
+
+            for (int testCounter = 0; testCounter<tests; testCounter++)
+            {
+                int counter = 0;
+                int jumpers = Convert.ToInt32(Console.ReadLine());
+                Point[,] points = new Point[2, jumpers];
+                for (int i = 0; i < jumpers; i++)
+                {
+                    int[] point_args = new int[2];
+                    string[] text = Console.ReadLine().Split(new char[] { ' ' });
+                    for (int j=0; j<2; j++)
+                    {
+                        point_args[j] = Convert.ToInt32(text[j]);
+                    }
+
+                    Point point = new Point(point_args[0], point_args[1]);
+                    points[0, i] = point;
+                }
+                for (int i = 0; i < jumpers; i++)
+                {
+                    int[] point_args = new int[2];
+                    string[] text = Console.ReadLine().Split(new char[] { ' ' });
+                    for (int j = 0; j < 2; j++)
+                    {
+                        point_args[j] = Convert.ToInt32(text[j]);
+                    }
+
+                    Point point = new Point(point_args[0], point_args[1]);
+                    points[1, i] = point;
+                }
+                for(int i=0; i<jumpers; i++)
+                {
+                    Algorithm algorithm = new Algorithm(points[0, i], points[1, i]);
+                    counter += algorithm.JumpsAlgorithm();
+                }
+                Console.WriteLine(counter.ToString());
+            }
         }
     }
 }
